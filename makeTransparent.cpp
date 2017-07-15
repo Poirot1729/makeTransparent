0dcfd7cd1e6b30fb21a22094ce0008dba697a27e
ebey97 <ebey97@gmail.com>
2017-07-15 18:43:37 +0530

    first commit

--- makeTransparent.cpp
+++ makeTransparent.cpp
@@ -9,28 +9,33 @@ int main(int argc, const char** argv)
 {
 	Mat img;
 	img = imread(argv[1],1);
-	int val1 = 0;
-	int val2 = 255;
-	if(!strcmp(argv[2],"black"))
-	{
-		val1 = 255;
-		val2 = 0;
-	}
+	
 	Mat res;
 	cvtColor(img,res,CV_BGR2BGRA);
-	for (int i = 0; i < res.rows; i++)
-   for (int j = 0; j < res.cols; j++)
-   {
-   	Vec4b& pixel = res.at<Vec4b>(i, j);
-      if (pixel[0] >= 240 && pixel[1] >= 240 && pixel[2] >= 240)
-      {
-      	pixel[3] = val1;
-      }
-      else
-      {
-      	pixel[3] = val2;
-      }
-   }
+	if(!strcmp(argv[2],"black"))
+	{
+		for (int i = 0; i < res.rows; i++)
+   	for (int j = 0; j < res.cols; j++)
+   	{
+   		Vec4b& pixel = res.at<Vec4b>(i, j);
+      	if (pixel[0] <= 20 && pixel[1] <= 20 && pixel[2] <= 20)
+      	{
+      		pixel[3] = 0;
+      	}
+   	}
+	}
+	else
+	{
+		for (int i = 0; i < res.rows; i++)
+   	for (int j = 0; j < res.cols; j++)
+   	{
+   		Vec4b& pixel = res.at<Vec4b>(i, j);
+      	if (pixel[0] >= 240 && pixel[1] >= 240 && pixel[2] >= 240)
+      	{
+      		pixel[3] = 0;
+      	}
+   	}
+	}
    imwrite("../res.png",res);
 	return 0;
 }
